dcd60417daa9864840019fd201d1159df297d709
John O Connor <johnpoconnor4@gmail.com>
2023-07-21 16:14:13 +0100

    made dynamic

--- src/app/users/get-users/page.tsx
+++ src/app/users/get-users/page.tsx
@@ -1,6 +1,7 @@
 import type { User } from "../api/route";
 import { prisma } from "@/app/lib/dbConnect";
 import ShowUsers from "./ShowUsers";
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return <ShowUsers />;
