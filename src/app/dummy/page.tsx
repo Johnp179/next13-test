e785ed5cd4437b258491e83fbf2d158025c497b8
searai <johnpoconnor4@gmail.com>
2023-08-15 18:11:00 +0100

    testing dynamic

--- src/app/dummy/page.tsx
+++ src/app/dummy/page.tsx
@@ -1,15 +1,5 @@
+export const dynamic = "force-dynamic";
+
 export default function Dummy() {
-  return (
-    <form method="post" action="/api/test">
-      <label className="block">
-        Username
-        <input name="email" type="text" className="text-black" />
-      </label>
-      <label className="block my-5">
-        Password
-        <input name="password" type="password" className="text-black" />
-      </label>
-      <button type="submit">Sign in</button>
-    </form>
-  );
+  return <h1>{Math.random()}</h1>;
 }
--- src/lib/process-env.ts
+++ src/lib/process-env.ts
@@ -1,8 +1,10 @@
 import { z } from "zod";
 
+const dev = process.env.NODE_ENV !== "production";
+
 const Validator = z.object({
   DATABASE_URL: z.string(),
-  NEXTAUTH_URL: z.string().optional(),
+  NEXTAUTH_URL: dev ? z.string().optional() : z.string(),
   NEXTAUTH_SECRET: z.string(),
   ENCRYPTION_KEY_BASE: z.string(),
   SALT: z.string(),
